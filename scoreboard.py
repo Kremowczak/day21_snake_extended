0b68383b26b1302ecc1e4b003c01a9619b0ce1be
Kremowczak <robert.jurasz62@gmail.com>
2026-03-30 14:11:23 +0200

    scoreboard implemented, and code refactor

---
+++ scoreboard.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.speed("fastest")
+        self.goto(-70,260)
+        self.write("Score: " + str(self.score), font=("Arial", 24, "bold"))
+        
+    def add_score(self):
+        self.score += 1
+        self.clear()
+        self.write("Score: " + str(self.score), font=("Arial", 24, "bold"))
+
+        
