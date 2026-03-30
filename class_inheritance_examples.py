d03d6dd31f86171adb87bbd83cd7ce3052b3b497
Kremowczak <robert.jurasz62@gmail.com>
2026-03-30 13:27:51 +0200

    first commit

---
+++ class_inheritance_examples.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+    def breathe(self):
+        print("Breathe")
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def breathe(self):
+        super().breathe()
+        print("under water")
+
+    def swim(self):
+        print("Swim")
+
+
+nemo = Fish()
+nemo.swim()
+nemo.breathe()
+print(nemo.num_eyes)
